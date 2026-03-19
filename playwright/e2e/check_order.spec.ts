3079d7719155905f0334afcf4846c2cd49fce1ef
Marcelo Nicolosi <celonicolosi@hotmail.com>
2026-03-19 20:32:55 +0000

    Validate the search order id flow using .toMatchAriaSnapshot

--- playwright/e2e/check_order.spec.ts
+++ playwright/e2e/check_order.spec.ts
@@ -57,13 +57,43 @@ test.describe('Check order status', ()=>{
       // await expect(orderCode).toBeVisible();
     
       // Second option by getByRole. + filter + locator
-      const containerPedido = page.getByRole('paragraph')
-        .filter({ hasText: /^Pedido$/ })
-        .locator('..') //Go up to the parent elemtn (the div that wrap both paragraphs)
+      // const containerPedido = page.getByRole('paragraph')
+      //   .filter({ hasText: /^Pedido$/ })
+      //   .locator('..') //Go up to the parent elemtn (the div that wrap both paragraphs)
     
-      await expect(containerPedido).toContainText('VLO-W4MS41');
+      // await expect(containerPedido).toContainText('VLO-W4MS41');
       
-      await expect(page.getByText('APROVADO')). toBeVisible();
+      // await expect(page.getByText('APROVADO')). toBeVisible();
+
+      // Approach using toMatchAriaSnapshot
+      await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+        - img
+        - paragraph: Pedido
+        - paragraph: ${order}
+        - img
+        - text: APROVADO
+        - img "Velô Sprint"
+        - paragraph: Modelo
+        - paragraph: Velô Sprint
+        - paragraph: Cor
+        - paragraph: Midnight Black
+        - paragraph: Interior
+        - paragraph: cream
+        - paragraph: Rodas
+        - paragraph: sport Wheels
+        - heading "Dados do Cliente" [level=4]
+        - paragraph: Nome
+        - paragraph: Marcelo Nicolosi
+        - paragraph: Email
+        - paragraph: marceleza@velo.dev
+        - paragraph: Loja de Retirada
+        - paragraph
+        - paragraph: Data do Pedido
+        - paragraph: /\\d+\\/\\d+\\/\\d+/
+        - heading "Pagamento" [level=4]
+        - paragraph: À Vista
+        - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+        `);
     });
     
     test('Should show a negative feedback when order is not found', async({page}) => {
